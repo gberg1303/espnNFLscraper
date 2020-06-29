7547e04975621580da409c01404d547259cd31b0
Jonathan Goldberg <gberg1303@aol.com>
2020-06-29 01:16:59 -0400

    Version 1

---
+++ R/get_game_ids.R
@@ -0,0 +1,80 @@
+#' @export
+get_game_ids <- function(season, season_type = c("preseason", "regular", "postseason")){
+  current_year <- as.double(substr(Sys.Date(), 1, 4))
+  espn_game_ids <- data.frame()
+
+  if (!season_type %in% c("preseason", "regular", "postseason", "all")) {
+    stop("Please choose season_type of 'regular',  'playoffs', 'postseason', or 'all'")
+  }
+
+  if (!dplyr::between(as.numeric(season), 2002, current_year)) {
+    stop(paste("Please choose season between 2002 and", current_year))
+  }
+
+  if(lubridate::month(Sys.Date()) < 12 & lubridate::month(Sys.Date()) > 2 & season_type == "postseason" & current_year == season | season_type == "postseason" & lubridate::month(Sys.Date()) <= 2 & current_year == season){
+    stop(paste("Unfortunately, the NFL Playoff Games have not been determined yet"))
+  }
+
+  message(
+    dplyr::if_else(
+      season_type == "regular",
+      glue::glue("Scraping from {season} {season_type} season!"),
+      glue::glue("Scraping from {season} {season_type}!")
+    )
+  )
+
+  season_type <- ifelse(season_type == "preseason", "1", season_type)
+  season_type <- ifelse(season_type == "regular", "2", season_type)
+  season_type <- ifelse(season_type == "postseason", "3", season_type)
+
+  weeks <- ifelse(season_type == "2", 17, 5)
+
+    for(week in 1:weeks){
+    url <- glue::glue("https://www.espn.com/nfl/schedule/_/week/{week}/year/{season}/seasontype/{season_type}")
+
+    webpage <- xml2::read_html(url)
+
+    links <- webpage %>%
+      rvest::html_nodes("a") %>%
+      rvest::html_attr("href")
+
+    espn_gameid <- links %>%
+      as.tibble() %>%
+      dplyr::filter(str_detect(value, "gameId") == TRUE) %>%
+      dplyr::pull(value) %>%
+      stringr::str_remove(., "/nfl/game/_/gameId/")
+
+    bye_teams <- webpage %>%
+      rvest::html_nodes(".odd.byeweek" ) %>%
+      rvest::html_nodes("abbr") %>%
+      rvest::html_text()
+
+    home_team <- webpage %>%
+      rvest::html_nodes(".home-wrapper") %>%
+      rvest::html_nodes("abbr") %>%
+      rvest::html_text()
+
+    away_team <- webpage %>%
+      rvest::html_nodes("abbr") %>%
+      rvest::html_text()
+    away_team <- away_team[!away_team %in% home_team]
+    away_team <- away_team[!away_team %in% bye_teams]
+
+    placeholder <- data.frame(
+      home_team,
+      away_team,
+      espn_gameid
+    ) %>%
+      dplyr::mutate(
+        season_type = season_type,
+        season = season,
+        week = week,
+        alt_gameid = paste0(season, "_", ifelse(week >= 10, paste0(week), paste0(0,week)), "_", away_team, "_", home_team)
+      )
+
+    espn_game_ids <- dplyr::bind_rows(espn_game_ids, placeholder)
+
+  }
+
+  return(espn_game_ids)
+}
---
+++ R/get_pregame_predictions.R
@@ -0,0 +1,56 @@
+#' Gets a single game's predictions from ESPN. Mainly used as a
+#'
+#' Gets a single game's predictions from ESPN. Mainly used as a helper for get_pregame_predictions
+prediction_helper <- function(espn_game_id){
+
+  pregame_predictions <- data.frame()
+
+  # Pull the JSon
+  game_json <- httr::GET(url = glue::glue("http://site.api.espn.com/apis/site/v2/sports/football/nfl/summary?event={espn_game_id}")) %>%
+    httr::content(as = "text", encoding = "UTF-8") %>%
+    jsonlite::fromJSON(flatten = TRUE)
+
+  # Pull the game data from the ID dataframe
+  pregame_predictions <- espn_nfl_ids %>%
+    dplyr::filter(espn_gameid == espn_game_id) %>%
+    dplyr::mutate(
+      home_team_wp = as.numeric(game_json[["predictor"]][["homeTeam"]][["gameProjection"]])
+    )
+
+  message(
+    paste("Pulling predictions for", pregame_predictions$alt_gameid)
+  )
+
+  # Grab and conver the Moneylines from Oddsmakers
+  if(!is.null(game_json[["pickcenter"]]) == TRUE){
+  vegas_odds <- data.frame(
+    providers = game_json[["pickcenter"]][["provider.name"]],
+    odds = ifelse(game_json[["pickcenter"]][["homeTeamOdds.moneyLine"]] > 0, 100/(game_json[["pickcenter"]][["homeTeamOdds.moneyLine"]] + 100), game_json[["pickcenter"]][["homeTeamOdds.moneyLine"]]/(game_json[["pickcenter"]][["homeTeamOdds.moneyLine"]]-100))
+    ) %>%
+    tidyr::pivot_wider(names_from = providers, values_from = odds)
+
+  pregame_predictions <- cbind(
+    pregame_predictions, vegas_odds
+  )
+
+  }
+
+  return(pregame_predictions)
+}
+
+
+#' Get Pregame Predictions
+#'
+#' Pull and combine pregame predictions from multiple games
+#' @param espn_gameids the gameids from espn
+#' @return dataframe with basic game info and pregame home team win probabilities
+#' @examples
+#' get_pregame_predictions(espn_nfl_ids %>% filter(season == 2019 & season_type == 2) %>% pull(espn_gameid))
+#' @export
+get_pregame_predictions <- function(espn_gameids){
+  purrr::map_df(espn_gameids, function(x){get_pregame_predictions_helper(x)})
+}
+
+
+
+
